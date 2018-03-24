d268405576495b9c12d84b1fb0ac7728747da940
anilkumarsandrapati <asandrap@tibco.com>
2018-03-24 15:07:27 +0530

    basic component creations with route and nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+// import { AngularFireModule } from 'angularfire2';
+// import { AngularFirestoreModule } from 'angularfire2/firestore';
+// import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ClientsComponent } from './component/clients/clients.component';
+import { ClientDetailsComponent } from './component/client-details/client-details.component';
+import { AddClientComponent } from './component/add-client/add-client.component';
+import { EditClientComponent } from './component/edit-client/edit-client.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { SettingsComponent } from './component/settings/settings.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // AngularFireModule.initializeApp(appRoutes),
+    // AngularFireDatabaseModule,
+    // AngularFireAuthModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
