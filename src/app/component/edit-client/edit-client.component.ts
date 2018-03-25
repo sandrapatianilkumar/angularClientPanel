894a3552c31f4fd229e679d54f2b51dd5a41e2be
sandrapatianilkumar <sandrapati.anilkumar@gmail.com>
2018-03-25 17:58:17 +0530

    edit form

--- src/app/component/edit-client/edit-client.component.ts
+++ src/app/component/edit-client/edit-client.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { timeout } from 'rxjs/operators';
+import {Client} from './../../models/Client';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import {ClientService} from './../../services/client.service';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-edit-client',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditClientComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0
+  };
+
+  disableBalanceOnEdit = true;
+  constructor(
+    public flashMessagesService : FlashMessagesService,
+    public clientService : ClientService,
+    public router : Router,
+    public route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    //get id
+    this.id = this.route.snapshot.params['id'];
+    //get client
+    this.clientService.getClient(this.id).subscribe(client => {
+      this.client = client;
+    });
+  }
+
+  onSubmit({value, valid}: {
+    value: Client,
+    valid: boolean
+  }) {
+    if (!valid) {
+      this
+        .flashMessagesService
+        .show('Please fill all the mandatory fields', {
+          cssClass: 'alert-danger',
+          timeout: 4000
+        });
+      this
+        .router
+        .navigate(['edit-client/' + this.id]);
+    } else {
+      this.clientService.updateClient(this.id, value);
+      this
+        .flashMessagesService
+        .show('Client updated', {
+          cssClass: 'alert-success',
+          timeout: 4000
+        });
+      this
+        .router
+        .navigate(['/client/' + this.id]);
+    }
   }
 
 }
