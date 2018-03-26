929ec33e3ac5a549fe61e5a932026871ee337de4
sandrapatianilkumar <sandrapati.anilkumar@gmail.com>
2018-03-26 23:48:11 +0530

    auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,62 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { environment } from '../environments/environment';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {environment} from '../environments/environment';
 import {FormsModule} from '@angular/forms';
 import {FlashMessagesModule} from 'angular2-flash-messages';
 
 //angular fire imports
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 
 //components
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './component/dashboard/dashboard.component';
-import { ClientsComponent } from './component/clients/clients.component';
-import { ClientDetailsComponent } from './component/client-details/client-details.component';
-import { AddClientComponent } from './component/add-client/add-client.component';
-import { EditClientComponent } from './component/edit-client/edit-client.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { SidebarComponent } from './component/sidebar/sidebar.component';
-import { LoginComponent } from './component/login/login.component';
-import { RegisterComponent } from './component/register/register.component';
-import { SettingsComponent } from './component/settings/settings.component';
-import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
+import {ClientsComponent} from './component/clients/clients.component';
+import {ClientDetailsComponent} from './component/client-details/client-details.component';
+import {AddClientComponent} from './component/add-client/add-client.component';
+import {EditClientComponent} from './component/edit-client/edit-client.component';
+import {NavbarComponent} from './component/navbar/navbar.component';
+import {SidebarComponent} from './component/sidebar/sidebar.component';
+import {LoginComponent} from './component/login/login.component';
+import {RegisterComponent} from './component/register/register.component';
+import {SettingsComponent} from './component/settings/settings.component';
+import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
 
 //service imports
 
-import { ClientService } from './services/client.service'
-import { AngularFireDatabase } from 'angularfire2/database-deprecated';
-import { AuthService } from './services/auth.service';
+import {ClientService} from './services/client.service'
+import {AngularFireDatabase} from 'angularfire2/database-deprecated';
+import {AuthService} from './services/auth.service';
+import {AuthGuard} from './guards/auth.guard';
 
-const appRoutes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'add-client', component: AddClientComponent },
-  {path: 'client/:id', component: ClientDetailsComponent},
-  {path: 'edit-client/:id', component: EditClientComponent}
+const appRoutes : Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  }, {
+    path: 'register',
+    component: RegisterComponent
+  }, {
+    path: 'login',
+    component: LoginComponent
+  }, {
+    path: 'add-client',
+    component: AddClientComponent,
+    canActivate: [AuthGuard]
+  }, {
+    path: 'client/:id',
+    component: ClientDetailsComponent,
+    canActivate: [AuthGuard]
+  }, {
+    path: 'edit-client/:id',
+    component: EditClientComponent,
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
@@ -57,9 +75,7 @@ const appRoutes: Routes = [
     PageNotFoundComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    FlashMessagesModule.forRoot(),
+    BrowserModule, FormsModule, FlashMessagesModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     RouterModule.forRoot(appRoutes)
   ],
@@ -69,8 +85,9 @@ const appRoutes: Routes = [
     AngularFireAuthModule,
     AngularFirestoreModule,
     ClientService,
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/RX';
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(
+        private router : Router, 
+        public afAuth : AngularFireAuth
+    ) {    }
+
+    canActivate(): Observable<boolean > {
+        return this.afAuth.authState.map(auth =>{
+            if (!auth) {
+                this.router.navigate(['/login']);
+                return false;
+            } else {
+                return true;
+            }
+        });
+    }
+}
